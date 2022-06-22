989709d5fccfe40be2f3938fad7a95b198d9e47a
yu.pan <panyu@pinpianyi.com>
2022-06-22 23:38:15 +0800

    feat:register PageLifecycleTrack in ioc container

--- packages/track-base/src/container/index.ts
+++ packages/track-base/src/container/index.ts
@@ -1,11 +1,18 @@
 import {Container} from 'inversify'
 import {SERVICE_IDENTIFIER} from '../constants'
-import {EventCenter, TrackEventDataProcess, TrackEventQueueManager} from '../impl'
-import type {TrackEventDataProcessInstance, TrackEventQueueManagerInterface, EventCenterInterface} from '../interface'
+import {EventCenter, TrackEventDataProcess, TrackEventQueueManager, PageLifecycleTrack} from '../impl'
+import type {
+    TrackEventDataProcessInstance,
+    TrackEventQueueManagerInterface,
+    EventCenterInterface,
+    PageLifecycleTrackInstance
+} from '../interface'
 
 const container = new Container()
 
 container.bind<EventCenterInterface>(SERVICE_IDENTIFIER.EVENT_CENTER).to(EventCenter).inSingletonScope()
+
+container.bind<PageLifecycleTrackInstance>(SERVICE_IDENTIFIER.PAGE_LIFECYCLE_TRACK).to(PageLifecycleTrack).inTransientScope()
 
 container.bind<TrackEventDataProcessInstance>(SERVICE_IDENTIFIER.TRACK_EVENT_DATA_PROCESS).to(TrackEventDataProcess).inTransientScope()
 
