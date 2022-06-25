1a51943940ee15f0a1eb75761ebf97110d6461ce
yu.pan <panyu@pinpianyi.com>
2022-06-26 03:23:50 +0800

    feat:modify export

--- packages/track-base/src/index.ts
+++ packages/track-base/src/index.ts
@@ -1,17 +1,28 @@
 import "reflect-metadata"
 import {noop} from './utils'
 import container from './container'
-import {SERVICE_IDENTIFIER} from './constants'
+import {SERVICE_IDENTIFIER, EVENT_TYPE, DEFAULT_EVENT_CONFIG, DEFAULT_EVENT_NAME} from './constants'
 import {EventCenter, TrackEventDataProcess, TrackEventQueueManager, PageLifecycleTrack} from './impl'
 import {initBaseTrack} from './initBaseTrack'
+
+import type {EventType, TargetTrackConfig, EventConfig} from './interface'
+
+export type {
+    EventType,
+    EventConfig,
+    TargetTrackConfig
+}
 
 export {
     noop,
     initBaseTrack,
-    container,
     EventCenter,
     TrackEventDataProcess,
     TrackEventQueueManager,
     PageLifecycleTrack,
-    SERVICE_IDENTIFIER
+    container,
+    SERVICE_IDENTIFIER,
+    EVENT_TYPE,
+    DEFAULT_EVENT_CONFIG,
+    DEFAULT_EVENT_NAME
 }
