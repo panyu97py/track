b19b90713978fbc61957c3f7a4bbcb283354ee6c
yu.pan <panyu@pinpianyi.com>
2022-07-03 01:38:46 +0800

    feat: init commitlint and husky

---
+++ commitlint.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    // https://juejin.cn/post/6983191125242675230
+    'type-enum': [2, 'always', [
+      'build', 'ci', 'perf', 'feat', 'fix', 'refactor', 'docs', 'chore', 'style', 'revert', 'test'
+    ]],
+  }
+}
