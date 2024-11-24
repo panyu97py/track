563479de75d65deeb05afddb7cbb4973f9e39c53
yu.pan <panyupy@vip.qq.com>
2024-11-24 17:40:28 +0800

    feat: 修改 eslint 配置

--- .eslintrc.js
+++ .eslintrc.js
@@ -15,6 +15,11 @@ module.exports = {
   rules: {
     '@typescript-eslint/no-empty-function': 0,
     '@typescript-eslint/no-explicit-any': 0,
-    '@typescript-eslint/no-var-requires': 0
+    '@typescript-eslint/no-var-requires': 0,
+    'no-use-before-define': ['error', {
+      variables: true,
+      functions: false,
+      classes: false // 允许类在被定义前使用
+    }]
   }
 }
